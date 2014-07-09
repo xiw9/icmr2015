5e7d5285440cf3156f9bd6eed9b03a36903e66c5
Xi Wang <wandsea@gmail.com>
2014-07-09 17:23:34 +0900

    ......

--- index.php
+++ index.php
@@ -117,19 +117,19 @@
     <hr class="featurette-divider">
     <div class="row">
       <div class="col-lg-4">
-        <img  src="img/notepad.png" style="width: 140px; height: 140px;">
+        <img  src="img/notepad.png" style="width: 100px; height: 100px;">
         <h2>Recently Updates</h2>
         <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
-        <img src="img/calendar.png" style="width: 140px; height: 140px;">
+        <img src="img/calendar.png" style="width: 100px; height: 100px;">
         <h2>Important Dates</h2>
         <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
-        <img src="img/direction.png" style="width: 140px; height: 140px;">
+        <img src="img/direction.png" style="width: 100px; height: 100px;">
         <h2>Time and Place</h2>
         <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
