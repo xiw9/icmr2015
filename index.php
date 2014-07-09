e9381acc2577f6021cf6895547694fc7311f0547
Xi Wang <wandsea@gmail.com>
2014-07-09 16:40:13 +0900

    ......

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="The Annual ACM International Conference on Multimedia Retrieval (ICMR)">
+  <meta name="author" content="BigVid Lab, Fudan University">
+  <meta name="keywords" content="Computer Science, Multimedia">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="img/favicon.ico" />
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/carousel.css" rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <title>ICMR 2015 | International Conference on Multimedia Retrieval</title>
+
+</head>
+<body>
+  <div class="navbar-wrapper">
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="logo navbar-btn pull-left" href="/" title="Home">
+            <img src="img/icmr-sm.png" alt="Home" />
+          </a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/header-1.jpg" alt="First slide">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>International Conference on Multimedia Retrieval 2015</h1>
+            <p>The annual ACM International Conference on Multimedia Retrieval will take place between June 23-26, 2015 in Shanghai, China. </p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/header-2.jpg" alt="Second slide" >
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>International Conference on Multimedia Retrieval 2015</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/header-3.jpg" alt="Third slide" >
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>International Conference on Multimedia Retrieval 2015</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+  </div>
+
+
+  <div class="container marketing">
+    <div class="row featurette">
+      <div class="col-md-8">
+        <h2 >About ICMR 2015</h2>
+        <p>Effectively and efficiently retrieving information based on user needs is one of the most exciting areas in multimedia research. The Annual ACM International Conference on Multimedia Retrieval (ICMR) offers a great opportunity for exchanging leading-edge multimedia retrieval ideas among researchers, practitioners and other potential users of multimedia retrieval systems.</p>
+        <p>This conference, puts together the long-lasting experience of former ACM CIVR and ACM MIR series, is set up to illuminate the state of the arts in multimedia (text, image, video and audio) retrieval.</p>
+      </div>
+      <div class="col-md-4">
+        <img class="featurette-image img-responsive" src="img/icmr.png" alt="ICMR 2015">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row">
+      <div class="col-lg-4">
+        <img  src="img/notepad.png" style="width: 140px; height: 140px;">
+        <h2>Recently Updates</h2>
+        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="img/calendar.png" style="width: 140px; height: 140px;">
+        <h2>Important Dates</h2>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="img/direction.png" style="width: 140px; height: 140px;">
+        <h2>Time and Place</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+    <div class="row featurette">
+      <div class="col-md-12">
+      <h2>Sponsers</h2>
+      <img src="img/acm_logo.gif" style="height: 80px; padding-right: 20px;">
+      <img src="img/fudan.png" style="height: 80px; padding-right: 20px;">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <footer>
+    <div class="footer">
+      <div class="container narrow row-fluid">
+        <p class="pull-right"><a href="#"><span class="glyphicon glyphicon-chevron-up"></span></a></p>
+        <p>&copy; 2015 ACM International Conference on Multimedia Retrieval (ICMR 2015)</p>
+        <p>Shanghai, China, June 23-26, 2015</p>
+      </div>
+    </div>
+  </footer>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,16 @@
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="The Annual ACM International Conference on Multimedia Retrieval (ICMR)">
+    <meta name="author" content="Xi Wang, BigVid Lab, Fudan University">
+    <meta name="keywords" content="Computer Science, Multimedia">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link href="css/metro-bootstrap.min.css" rel="stylesheet">
+    <link href="css/metro-bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/iconFont.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.mousewheel.min.js"></script>
+    <script src="js/metro.min.js"></script>
+    <link rel="shortcut icon" href="img/favicon.ico" />
