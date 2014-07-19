9598c31d8bdfd2ea12edbfcbe6e25d5c14a4421d
Xi Wang <wandsea@gmail.com>
2014-07-19 19:23:52 +0900

    sidebar.php

--- cfp.php
+++ cfp.php
@@ -55,17 +55,7 @@
         </div>  
       </div>
       <div class="col-md-3">
-        <div class="panel panel-primary">
-          <div class="panel-heading">
-            <h1 class="panel-title">Sponsors</h1>
-          </div>
-          <div class="panel-body" >
-            <p class="text-center">
-            <img src="img/acm_logo.gif" style="height: 80px; padding-right: 20px;">
-            <img src="img/fudan.png" style="height: 80px; padding-right: 20px;">
-            </p>
-          </div>
-        </div>
+      <?php include 'php/sidebar.php' ?>
       </div>
     </div>
     <hr class="featurette-divider">
