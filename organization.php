357e283a9c95aaf8ffee62aa899e719330447d26
Xi Wang <wandsea@gmail.com>
2014-08-05 16:34:34 +0800

    <hr>

--- organization.php
+++ organization.php
@@ -23,28 +23,30 @@
                   <p><a href="http://vireo.cs.cityu.edu.hk/">Chong-Wah Ngo</a>, City University of Hong Kong, China</p> 
 		  <p><a href="http://www.cs.cmu.edu/~alex/">Alex Hauptmann</a>, Carnegie Mellon University, USA</p>
 		  <p><a href="http://scholar.google.com/citations?user=DTbhX6oAAAAJ&hl=en">Xiangyang Xue</a>, Fudan University, Shanghai, China</p>
-		  
+		  <hr>
 		  <h4>Technical Program Co-Chairs</h4>
 		  <p><a href="http://www.ceessnoek.info/">Cees Snoek</a>, Unversity of Amsterdam, The Netherlands</p>
 		  <p><a href="http://www.svcl.ucsd.edu/~nuno/">Nuno Vasconcelos</a>, University of California, San Diego, USA</p>
 		  <p><a href="http://www.yugangjiang.info/">Yu-Gang Jiang</a>, Fudan University, Shanghai, China</p>
-		  
+		  <hr>
 		  <h4>Brave New Idea Chairs</h4>
 		  <p>Jibe Luo, University of Rochester, USA</p>
 		  <p>Marcel Worring, University of Amsterdam, The Netherlands</p>
-		  
+		  <hr>
 		  <h4>Practitioner Chairs</h4>
 		  <p>R. Manmatha, <a href="http://a9.com/">A9.com</a>, USA</p>
 		  <p>Yiannis Kompatsiaris, CERTH/ITI, Greece</p>
 		  <p>Dong Wang, Central Research Lab, Huawei Technologies, China</p>
+		  <hr>
 		  <h4>Special Session Chairs</h4>
 		  <p>Winston Hsu, National Taiwan University</p>
 		  <p>Nicu Sebe, University of Trento, Italy</p>
 		  <p>Jinhui Tang, Nanjing University of Science and Technology, China</p>
+		  <hr>
 		  <h4>Workshop Chairs</h4>
 		  <p>Shin'ichi Satoh, NII, Japan</p>
 		  <p>Qi Tian, UTSA, USA</p>
-		  
+		  <hr>
 		  <h4>Panel Chairs</h4>
 		  <p>Tat-Seng Chua, NUS, Singapore</p>
 		  <p>Lexing Xie, ANU, Australia</p>
@@ -54,36 +56,36 @@
 		  <h4>Doctoral Symposium Chairs</h4>
 		  <p>Xiao-Yong Wei, Sichuan University, China</p>
 		  <p>Subh Bhattacharya, Siemens Research, USA</p>
-		
+		  <hr>
 		  <h4>Tutorial Chairs</h4>
 		  <p>Marco Bertini, University of Florence, Italy</p>
 		  <p>Junsong Yuan, NTU, Singapore</p>  
-		  
+		  <hr>
 		  <h4>Demo Chairs</h4>
 		  <p>Benoit Huet, Eurecom, France</p>
 		  <p>Yantao Zheng, Google, USA</p>
-		  
+		  <hr>
 		  <h4>Publicity Chairs</h4>
 		  <p>Ling Guan, Ryerson University, Canada</p>
 		  <p>Richang Hong, Hefei University of Technology, China</p>
 		  <p>Vasileios Mezaris, Center for Research and Technology Hellas, Greece</p>
-		  
+		  <hr>
 		  <h4>Sponsorship Chairs</h4>
 		  <p>Peng Cui, Tsinghua University, China</p>
 		  <p>Wu Xiao, Southwest Jiaotong University, China</p>
-		  
+		  <hr>
 		  <h4>Publication Chairs</h4>
 		  <p>Xirong Li, Renmin University, China</p>
 		  <p>Xiangdong Zhou, Fudan University, China</p>
-		  
+		  <hr>
 		  <h4>Finance Chair</h4>
 		  <p>Hong Lu, Fudan University, China</p>
-		  
+		  <hr>
 		  <h4>Local Chairs</h4>
 		  <p>Feng Wang, East China Normal University, China</p>
 		  <p>Hanli Wang, Tongji University, China</p>
 		  <p>Cheng Jin, Fudan University, China</p>
-		  
+		  <hr>
 		  <h4>Web Chair</h4>
 		  <p>Xi Wang, Fudan University, Shanghai, China</p>
 		</div>
