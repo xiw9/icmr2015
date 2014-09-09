c0575bb4702abc21879b2369f005493721c43096
Xi Wang <xiwfdu@gmail.com>
2014-09-09 10:13:11 +0800

    user take note

--- submission_instructions.php
+++ submission_instructions.php
@@ -43,6 +43,8 @@
             </ul>
             <h3>Submissions</h3>
             <p>Submissions should be made through the <a>EasyChair conference website (TBA).</a></p>
+            <h3>AUTHORS TAKE NOTE:</h3>
+            <p>The official publication date is the date the proceedings are made available in the ACM Digital Library. This date may be up to two weeks prior to the first day of your conference. The official publication date affects the deadline for any patent filings related to published work. (For those rare conferences whose proceedings are published in the ACM Digital Library after the conference is over, the official publication date remains the first day of the conference.)</p>
 
           </div>
         </div>  
