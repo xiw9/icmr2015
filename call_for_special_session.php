2c54068eacaac860ef18b5e5473b91970872827e
Xi Wang <wandsea@gmail.com>
2014-09-26 08:49:44 +0800

    call_for_special_session

---
+++ call_for_special_session.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Call for Special Session Proposals | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Call for Special Session Proposals</h1>
+          </div>
+          <div class="panel-body" >
+            <p>ICMR 2014 will include some Special Sessions for innovative and frontier topics in the field of multimedia retrieval. Each Special Session will include around 5 papers. A proposal for the Special Sessions should include:</p>
+            <ul>
+              <li>Title of proposed session</li>
+              <li>An introduction stating the importance of the topic and the rationale for the proposed session</li>
+              <li>Organizers (including brief bio and contact information)</li>
+              <li>Tentative/Confirmed list of invited papers (titles/affiliations/authors)</li>
+            </ul>
+
+            <p>Proposals will be evaluated considering the appropriateness with the conference main topics, as well as the quality of the paper abstracts. Papers will be submitted to the submission system and will be subjected to the same peer review of regular papers. Special sessions organizers will be responsible for the review management of the submitted papers. All papers should comply with the same template used for regular papers. The organizers send their recommendations to the Special Session Co-Chairs for final approval.  If a significant number of the papers are rejected after peer-review for an approved Special Session, this Special Session may be cancelled. If a proposal is not accepted, papers in that proposal can still be submitted to the regular sessions.</p>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
