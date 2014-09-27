20c02d45ce7c0449e97a155b69e9b8869a7695d5
Xi Wang <xiwfdu@gmail.com>
2014-09-27 17:23:12 +0800

    special session

--- call_for_special_session.php
+++ call_for_special_session.php
@@ -24,8 +24,20 @@
               <li>Organizers (including brief bio and contact information)</li>
               <li>Tentative/Confirmed list of invited papers (titles/affiliations/authors)</li>
             </ul>
-
             <p>Proposals will be evaluated considering the appropriateness with the conference main topics, as well as the quality of the paper abstracts. Papers will be submitted to the submission system and will be subjected to the same peer review of regular papers. Special sessions organizers will be responsible for the review management of the submitted papers. All papers should comply with the same template used for regular papers. The organizers send their recommendations to the Special Session Co-Chairs for final approval.  If a significant number of the papers are rejected after peer-review for an approved Special Session, this Special Session may be cancelled. If a proposal is not accepted, papers in that proposal can still be submitted to the regular sessions.</p>
+            <h3>Proposal Submission </h3>
+            <p>Those who are interested in organizing a Special Session should submit a proposal adhering to the above requirements by email to all Special Session Chairs. All the papers in the Special Sessions should follow the same style as the regular ICMR papers. </p>
+            <h3>Important Dates</h3>
+            <p>December 1, 2014:  Proposal for special session</p>
+            <p>December 15, 2014: Decision for special session proposals</p>
+            <p>January 22, 2015: Special session paper submission</p>
+            <p>March 15, 2015:    Notification of acceptance</p>
+            <p>April 5, 2015:     Camera-ready paper due</p>
+            <h3>Special Session Chairs</h3>
+            <p>Winston Hsu (<a href="mailto:whsu@ntu.edu.tw">whsu@ntu.edu.tw</a>), National Taiwan University</p>
+            <p>Nicu Sebe (<a href="mailto:sebe@disi.unitn.it">sebe@disi.unitn.it</a>), University of Trento</p>
+            <p>Jinhui Tang (<a href="mailto:tangjh1981@gmail.com">tangjh1981@gmail.com</a>), Nanjing Univ. of Sci. and Tech</p>
+
           </div>
         </div>  
       </div>
