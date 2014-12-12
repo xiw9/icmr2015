90c684816d910baf47720702ff96ca8b2d08c77f
Xi Wang <wandsea@gmail.com>
2014-12-12 15:04:53 +0800

    sidebar

--- php/sidebar.php
+++ php/sidebar.php
@@ -4,7 +4,7 @@
               <h1 class="panel-title">Sponsors and Partners</h1>
             </div>
             <div class="panel-body" >
-              <p class="text-center">
+              <p class="text-center" style="max-width: 300px">
                 <img class="img-responsive" src="img/acm_logo.gif" style="margin-bottom: 20px;">
                 <img class="img-responsive" src="img/sigmm.jpg" style="margin-bottom: 20px;">
                 <img src="img/fudan.png" style="max-height: 80px; margin-bottom: 20px;">
