866c0d1921dc9226648a6cec914feebb3fa34227
Xi Wang <wandsea@gmail.com>
2015-03-16 09:08:36 +0800

    accepted

---
+++ accepted_papers.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Accepter Papers | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Accepter Papers</h1>
+          </div>
+          <div class="panel-body" >
+            <h3>Full Papers</h3>
+            <pre>1  A Privacy-Preserving Bipartite Graph Matching Framework for Multimedia Analysis and Retrieval
+15  Image Classification and Retrieval are ONE
+18  Describing Images with Hierarchical Concepts and Object Class Localization
+23  Supervised Multi-scale Locality Sensitive Hashing
+26  Nonnegative Sparse Neighborhood Propagation
+29  Fast Democratic Aggregation and Query Fusion for Image Search
+33  Bundling Centre For Landmark Image Discovery
+49  Fine-Grained Image Categorization by Localizing Tiny Object Parts from Unannotated Images
+51  DeepIndex for Accurate and Efficient Image Retrieval
+53  A Novel Visual-Region-Descriptor-based Approach to Sketch-based Image Retrieval
+58  Hierarchical Encoding of Binary Descriptors for Image Matching
+69  Scalable Multimodal Search with Distributed Indexing by Sparse Hashing
+71  Facial Action Unit Classification with Hidden Knowledge under Incomplete Annotation
+73  MInsight in Image Collections by Multimedia Pivot Tables
+82  Distribution Regularized Nonnegative Matrix Factorization for Transfer Visual Feature Learning
+83  Robust and Discriminative Concept Factorization for Image Representation
+94  Heterogeneous Semantic Level Features Fusion for Action Recognition
+101 Social Friend Recommendation Based on Network Correlation and Feature Co-Clustering
+111 Kernelizing Spatially Consistent Visual Matches for Fine-Grained Classification
+118 Extracting 3D Trajectories of Objects from 2D Videos using Particle Filter
+123 Space-time histograms and their application to person re-identification in TV shows
+125 Improving Diversity in Image Search via Supervised Relevance Scoring
+126 Unsupervised Distance Learning by Rank Correlation Measures for Image Retrieval
+127 Effective, Efficient, and Scalable Unsupervised Distance Learning in Image Retrieval Tasks
+130 Exploring Pooling Strategies based on Idiosyncrasies of Spatio-Temporal Interest Points
+131 Temporal-Order Preserved Dynamic Quantization for Human Action Recognition from Multimodal Sensor Streams
+133 Image-Text Cross-Modal Retrieval via Modality-Specific Feature Learning
+136 Unified YouTube Video Recommendation via Cross-network Collaboration
+138 Twin Feature and Similarity Maximal Matching for Image Retrieval
+158 Location-Based Parallel Tag Completion for Geo-tagged Social Photo Retrieval
+161 Exploiting Spatial Relationship between Scenes for Hierarchical Video Geotagging
+173 Fusing Pointwise and Pairwise Labels for Supporting Personalized Image Retrieval
+178 USING VIEWER’S FACIAL EXPRESSION AND HEART RATE FOR SPORTS VIDEO HIGHLIGHTS DETECTION
+189 A Deep Neural Network for Modeling Music
+201 Robust Seed Detection and Growing with Deep Convolutional Features for Scene Text Detection
+202 High-Dimensional Indexing by Sparse Approximation
+213 To Keep or not to Keep: An Expectation-oriented Photo Selection Method for Personal Photo Collections
+214 Swap Retrieval: Retrieving images of cats when the query shows a dog
+222 Diffusion-on-Manifold Aggregation of Local Features for Shape-based 3D Model Retrieval
+224 Graph Learning on K Nearest Neighbours for Automatic Image Annotation
+227 Scalable organization of collections of motion capture data via quantitative and qualitative analysis
+240 Content-Based Video Search over 1 Million Videos with 1 Core in 1 Second
+241 Bridging the Ultimate Semantic Gap: A Semantic Search Engine for Internet Videos
+246 Encoding Concept Prototypes for Video Event Detection and Summarization
+247 Discovering Semantic Vocabularies for Cross-Media Retrieval
+248 Bag-of-Fragments: Selecting and encoding video fragments for event detection and recounting
+249 Latent Factors of Visual Popularity Prediction
+251 Building A Deep Learning System for Video Classification: An In-depth Study           </pre>
+          <h3>Short Papers</h3>
+          <pre>3  Content-based Image Retrieval Using Rotation-invariant Histograms of Oriented Gradients
+4 Augmented Feature Fusion for Image Retrieval System
+37  Accio: A Data Set for Face Track Retrieval in Movies Across Age
+38  A Two-step Approach to Cross-modal Hashing
+39  Cross-Scenario Eyeglasses Retrieval via EGYPT Model
+57  Discovering the Latent Similarities of the KNN Graph by Metric Transformation
+59  Formation period matters: Towards socially consistent group detection via dense subgraph seeking
+60  Memory vectors for particular object retrieval with multiple queries
+62  Semantic-aware Hashing for Social Image Retrieval
+67  Zero-shot Image Categorization via Image Correlation Exploration
+76  Deep Bottleneck Feature for Image Classification
+78  Maximally Visual-Homogeneous Region Detector for Large Scale Image Retrieval
+87  Rapid Clothing Retrieval via Deep Learning of Binary Codes and Hierarchical Search
+97  Discriminative Latent Feature Space Learning for Cross-Modal Retrieval
+103 Image Retrieval by User-oriented Ranking
+105 Spatial Constraint for Image Location Estimation
+117 Large Scale Image Annotation via Deep Representation Learning and Tag Embedding Learning
+124 Probabilistic Matrix Factorization With Semantic And Visual Neighborhoods For Image Tag Completion
+134 CRMActive : An Active Learning Based Approach For Effective Video Annotation And Retrieval
+135 Personalized Egocentric Video Summarization for Cultural Experience
+142 Specific Person Retrieval via Incomplete Text Description
+146 Combining generic and specific information for cross-modal retrieval
+148 3D Sketch-Based 3D Model Retrieval
+151 Boosting Prediction of Geo-location for Web Images Through Integrating Multiple Knowledge Sources
+162 Expression Recognition from Visible Images with the Help of Thermal Images
+164 Multi-facet Learning using Deep Convolutional Neural Network for Person-Related Categories in Photos
+176 Sketch-based Image Retrieval via Shape Words
+180 Multiple Aesthetic Attribute Assessment by Exploiting Relations Among Aesthetic Attributes
+181 Emotion recognition from users' eeg signals using hierarchical bayesian model with privileged information
+221 Kernel Local Descriptors with Implicit Rotation Matching
+232 Audio-Based Multimedia Event Detection with DNNs and Sparse Sampling
+236 Learning Binary Codes for Hashing via Feature Decomposition
+243 Multimodal Learning with Deep Boltzmann Machine for Emotion Prediction in User Generated Videos
+244 Improving Automatic Name-Face Association using Celebrity Images on the Web</pre>
+          <h3>Demos</h3>
+          <pre>100  Music Positioning and Annotation For Television Videos
+149 KinectSBR: A Kinect-Assisted 3D Sketch-Based 3D Model Retrieval System
+153 An Improved System For Real-Time Scene Text Recognition
+198 DigInPix: visual named-entities identification in images and videos
+256 Mobile Media Thumbnailing
+257 IdeaPanel: A Large Scale Interactive Sketch-based Image Search System
+258 A Sparse Ensemble Learning System For Efficient Semantic Indexing
+260 A Multi-Sensory Gesture-Based Occupational Therapy Environment for Controlling Home Appliances
+261 Incremental Multimodal Query Construction for Video Search</pre>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
