da2e96e8212120189d92144afb7a0b7a7832ecc7
Xi Wang <wandsea@gmail.com>
2015-03-27 14:28:17 +0800

    ...

--- program.php
+++ program.php
@@ -20,7 +20,7 @@
             <p>Program at a glance:</p>
 <table class="table-program table table-hover table-bordered  ">
 <tr class="info"><th>Venue</th><th class="text-center">Fudan University</th><th colspan="3" class="text-center">International Convention Center</th></tr>
- <tr><td>Date</td><td>June 23 Tuesday (Day 0)</td><td>June 24 Wednesday (Day 1)</td><td>June 25 Thursday (Day 2)</td><td>June 26 Friday (Day 3)</td></tr>
+ <tr><td>Date</td><td>June 23 Tuesday Day 0</td><td>June 24 Wednesday Day 1</td><td>June 25 Thursday Day 2</td><td>June 26 Friday Day 3</td></tr>
  <tr><td>&nbsp;</td><td>Tutorials & Workshops</td><td>Main Conference</td><td>Main Conference</td><td>Main Conference & Practitioner Day</td></tr>
  <tr><td>08:30am</td><td>&nbsp;</td><td>Registration</td><td>&nbsp;</td><td>&nbsp;</td></tr>
  <tr><td>08:45am</td><td>Registration</td><td>Welcome and Introduction</td><td>Registration</td><td>Registration</td></tr>
@@ -39,7 +39,7 @@
  <tr><td>2:20pm</td><td>&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td></tr>
  <tr><td>2:40pm</td><td>&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td></tr>
  <tr><td>3:00pm</td><td>&nbsp;</td><td>Coffee Break</td><td>Coffee Break</td><td>Coffee Break </td></tr>
- <tr><td>3:20pm</td><td>&nbsp;</td><td class="success">Oral 3: Concepts for MR</td><td class="info">Panel Discussion</td><td class="danger">Full Paper Posters & Special Poster Session</td></tr>
+ <tr><td>3:20pm</td><td>&nbsp;</td><td class="success">Oral 3: Concepts</td><td class="info">Panel Discussion</td><td class="danger">Full Paper Posters & Special Poster Session</td></tr>
  <tr><td>3:40pm</td><td>&nbsp;</td><td class="success">4 papers</td><td class="info">TBA</td><td class="danger">&nbsp;</td></tr>
  <tr><td>4:00pm</td><td>&nbsp;</td><td class="success">&nbsp;</td><td class="info">&nbsp;</td><td class="danger">&nbsp;</td></tr>
  <tr><td>4:20pm</td><td>&nbsp;</td><td class="success">&nbsp;</td><td class="info">&nbsp;</td><td class="danger">&nbsp;</td></tr>
@@ -50,48 +50,48 @@
  <tr><td>6:00pm</td><td>&nbsp;</td><td class="danger">&nbsp;</td><td>&nbsp;</td><td>Conference Close</td></tr>
  <tr><td>6:20pm</td><td>&nbsp;</td><td class="danger">&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
  <tr><td>6:40pm</td><td>&nbsp;</td><td class="danger">&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
- <tr height="50"><td>7:00pm<br/>--<br/>late</td><td>&nbsp;</td><td>Reception: Int'l Convention Center</td><td>Banquet: venue TBA</td><td>&nbsp;</td></tr>
+ <tr height="50"><td>7:00pm<br/>--<br/>late</td><td>&nbsp;</td><td>Reception: <br/>International Convention Center</td><td>Banquet: <br/>venue TBA</td><td>&nbsp;</td></tr>
 </table>
 <hr>
 <p>Detailed Program:</p>
 <table class="table-program table table-hover table-bordered">
- <tr><th colspan="2" >Best paper session</th></tr>
- <tr><td>241</td><td>Bridging the Ultimate Semantic Gap: A Semantic Search Engine for Internet Videos. Lu Jiang, Shoou-I Yu, Deyu Meng, Teruko Mitamura and Alex Hauptmann.</td></tr>
- <tr><td>136</td><td>Unified YouTube Video Recommendation via Cross-network Collaboration. Ming Yan, Jitao Sang and Changsheng Xu.</td></tr>
- <tr><td>15</td><td>Image Classification and Retrieval are ONE. Lingxi Xie, Richang Hong, Bo Zhang and Qi Tian.</td></tr>
- <tr><td>74</td><td>Social Event Mining in Large Photo Collections. Maia Zaharieva, Matthias Zeppelzauer, Manfred Del Fabro and Daniel Schopfhauser.</td></tr>
+ <tr><th colspan="2" >Best Paper Session</th></tr>
+ <tr><td>241</td><td>Bridging the Ultimate Semantic Gap: A Semantic Search Engine for Internet Videos. <br/>Lu Jiang, Shoou-I Yu, Deyu Meng, Teruko Mitamura and Alex Hauptmann.</td></tr>
+ <tr><td>136</td><td>Unified YouTube Video Recommendation via Cross-network Collaboration. <br/>Ming Yan, Jitao Sang and Changsheng Xu.</td></tr>
+ <tr><td>15</td><td>Image Classification and Retrieval are ONE. <br/>Lingxi Xie, Richang Hong, Bo Zhang and Qi Tian.</td></tr>
+ <tr><td>74</td><td>Social Event Mining in Large Photo Collections. <br/>Maia Zaharieva, Matthias Zeppelzauer, Manfred Del Fabro and Daniel Schopfhauser.</td></tr>
  <tr><th colspan="2" >Oral 1: Image Retrieval </th></tr>
- <tr><td>173</td><td>Fusing Pointwise and Pairwise Labels for Supporting Personalized Image Retrieval.Lin Chen and Baoxin Li.</td></tr>
- <tr><td>127</td><td>Effective, Efficient, and Scalable Unsupervised Distance Learning in Image Retrieval Tasks. Lucas Valem, Daniel C. G. Pedronette, Ricardo Torres, Edson Borin and Jurandy Almeida. </td></tr>
- <tr><td>138</td><td>Twin Feature and Similarity Maximal Matching for Image Retrieval. Lei Wang, Hanli Wang and Fengkuangtian Zhu. </td></tr>
- <tr><td>29</td><td>Fast Democratic Aggregation and Query Fusion for Image Search. Zhanning Gao, Jianru Xue, Wengang Zhou, Shanmin Pang and Qi Tian. </td></tr>
- <tr><td>51</td><td>DeepIndex for Accurate and Efficient Image Retrieval. Yu Liu and Michael S. Lew. </td></tr>
+ <tr><td>173</td><td>Fusing Pointwise and Pairwise Labels for Supporting Personalized Image Retrieval.<br/>Lin Chen and Baoxin Li.</td></tr>
+ <tr><td>127</td><td>Effective, Efficient, and Scalable Unsupervised Distance Learning in Image Retrieval Tasks. <br/>Lucas Valem, Daniel C. G. Pedronette, Ricardo Torres, Edson Borin and Jurandy Almeida. </td></tr>
+ <tr><td>138</td><td>Twin Feature and Similarity Maximal Matching for Image Retrieval. <br/>Lei Wang, Hanli Wang and Fengkuangtian Zhu. </td></tr>
+ <tr><td>29</td><td>Fast Democratic Aggregation and Query Fusion for Image Search. <br/>Zhanning Gao, Jianru Xue, Wengang Zhou, Shanmin Pang and Qi Tian. </td></tr>
+ <tr><td>51</td><td>DeepIndex for Accurate and Efficient Image Retrieval. <br/>Yu Liu and Michael S. Lew. </td></tr>
  <tr><th colspan="2">Oral 2: Person and Objects</th></tr>
- <tr><td>118</td><td>Extracting 3D Trajectories of Objects from 2D Videos using Particle Filter. Zeyd Boukhers, Kimiaki Shirahama, Frédéric Li and Marcin Grzegorzek. </td></tr>
- <tr><td>71</td><td>Facial Action Unit Classification with Hidden Knowledge under Incomplete Annotation. Jun Wang, Shangfei Wang and Qiang Ji.</td></tr>
- <tr><td>131</td><td>Temporal-Order Preserved Dynamic Quantization for Human Action Recognition from Multimodal Sensor Streams. Jun Ye, Guo-Jun Qi and Kien Hua. </td></tr>
- <tr><td>123</td><td>Space-time histograms and their application to person re-identification in TV shows. Rémi Auguste, Jean Martinet and Pierre Tirilly. </td></tr>
+ <tr><td>118</td><td>Extracting 3D Trajectories of Objects from 2D Videos using Particle Filter. <br/>Zeyd Boukhers, Kimiaki Shirahama, Frédéric Li and Marcin Grzegorzek. </td></tr>
+ <tr><td>71</td><td>Facial Action Unit Classification with Hidden Knowledge under Incomplete Annotation. <br/>Jun Wang, Shangfei Wang and Qiang Ji.</td></tr>
+ <tr><td>131</td><td>Temporal-Order Preserved Dynamic Quantization for Human Action Recognition from Multimodal Sensor Streams. <br/>Jun Ye, Guo-Jun Qi and Kien Hua. </td></tr>
+ <tr><td>123</td><td>Space-time histograms and their application to person re-identification in TV shows. <br/>Rémi Auguste, Jean Martinet and Pierre Tirilly. </td></tr>
  <tr><th colspan="2">Oral 3: Concepts</th></tr>
- <tr><td>246</td><td>Encoding Concept Prototypes for Video Event Detection and Summarization. Masoud Mazloom, Amirhossein Habibian, Dong Liu, Cees Snoek and Shih-Fu Chang. </td></tr>
- <tr><td>83</td><td>Robust and Discriminative Concept Factorization for Image Representation. Yuchen Guo and Guiguang Ding. </td></tr>
- <tr><td>49</td><td>Fine-Grained Image Categorization by Localizing Tiny Object Parts from Unannotated Images. Luming Zhang, Yi Yang and Roger Zimmermann. </td></tr>
- <tr><td>247</td><td>Discovering Semantic Vocabularies for Cross-Media Retrieval. Amirhossein Habibian, Thomas Mensink and Cees Snoek. </td></tr>
- <tr><th colspan="2">Oral 4: Analytical</th></tr>
- <tr><td>222</td><td>Diffusion-on-Manifold Aggregation of Local Features for Shape-based 3D Model Retrieval. Takahiko Furuya and Ryutarou Ohbuchi. </td></tr>
- <tr><td>26</td><td>Nonnegative Sparse Neighborhood Propagation. Zhao Zhang, Mingbo Zhao, Li Zhang and Li Fanzhang. </td></tr>
- <tr><td>202</td><td>High-Dimensional Indexing by Sparse Approximation. Pedro Borges, André Mourão and Joao Magalhaes. </td></tr>
- <tr><td>111</td><td>Kernelizing Spatially Consistent Visual Matches for Fine-Grained Classification. Valentin Leveau, Alexis Joly, Olivier Buisson and Patrick Valduriez.</td></tr>
- <tr><td>58</td><td>Hierarchical Encoding of Binary Descriptors for Image Matching. Zhendong Mao, Yongdong Zhang and Qi Tian. </td></tr>
+ <tr><td>246</td><td>Encoding Concept Prototypes for Video Event Detection and Summarization. <br/>Masoud Mazloom, Amirhossein Habibian, Dong Liu, Cees Snoek and Shih-Fu Chang. </td></tr>
+ <tr><td>83</td><td>Robust and Discriminative Concept Factorization for Image Representation. <br/>Yuchen Guo and Guiguang Ding. </td></tr>
+ <tr><td>49</td><td>Fine-Grained Image Categorization by Localizing Tiny Object Parts from Unannotated Images. <br/>Luming Zhang, Yi Yang and Roger Zimmermann. </td></tr>
+ <tr><td>247</td><td>Discovering Semantic Vocabularies for Cross-Media Retrieval. <br/>Amirhossein Habibian, Thomas Mensink and Cees Snoek. </td></tr>
+ <tr><th colspan="2">Oral 4: Analytical Methods for MR</th></tr>
+ <tr><td>222</td><td>Diffusion-on-Manifold Aggregation of Local Features for Shape-based 3D Model Retrieval. <br/>Takahiko Furuya and Ryutarou Ohbuchi. </td></tr>
+ <tr><td>26</td><td>Nonnegative Sparse Neighborhood Propagation. <br/>Zhao Zhang, Mingbo Zhao, Li Zhang and Li Fanzhang. </td></tr>
+ <tr><td>202</td><td>High-Dimensional Indexing by Sparse Approximation. <br/>Pedro Borges, André Mourão and Joao Magalhaes. </td></tr>
+ <tr><td>111</td><td>Kernelizing Spatially Consistent Visual Matches for Fine-Grained Classification. <br/>Valentin Leveau, Alexis Joly, Olivier Buisson and Patrick Valduriez.</td></tr>
+ <tr><td>58</td><td>Hierarchical Encoding of Binary Descriptors for Image Matching. <br/>Zhendong Mao, Yongdong Zhang and Qi Tian. </td></tr>
  <tr><th colspan="2">Oral 5: Photo Applications</th></tr>
- <tr><td>33</td><td>Bundling Centre For Landmark Image Discovery. Qian Zhang and Guoping Qiu. </td></tr>
- <tr><td>253</td><td>Visual Event Summarization on Social Media using Topic Modelling and Graph-based Ranking Algorithms. Manos Schinas, Symeon Papadopoulos, Yiannis Kompatsiaris and Pericles Mitkas. </td></tr>
- <tr><td>213</td><td>To Keep or not to Keep: An Expectation-oriented Photo Selection Method for Personal Photo Collections. Andrea Ceroni, Vassilios Solachidis, Claudia Niederée, Olga Papadopoulou, Nattiya Kanhabua and Vasileios Mezaris. </td></tr>
- <tr><td>249</td><td>Latent Factors of Visual Popularity Prediction. Spencer Cappallo, Thomas Mensink and Cees Snoek. </td></tr>
+ <tr><td>33</td><td>Bundling Centre For Landmark Image Discovery. <br/>Qian Zhang and Guoping Qiu. </td></tr>
+ <tr><td>253</td><td>Visual Event Summarization on Social Media using Topic Modelling and Graph-based Ranking Algorithms. <br/>Manos Schinas, Symeon Papadopoulos, Yiannis Kompatsiaris and Pericles Mitkas. </td></tr>
+ <tr><td>213</td><td>To Keep or not to Keep: An Expectation-oriented Photo Selection Method for Personal Photo Collections. <br/>Andrea Ceroni, Vassilios Solachidis, Claudia Niederée, Olga Papadopoulou, Nattiya Kanhabua and Vasileios Mezaris. </td></tr>
+ <tr><td>249</td><td>Latent Factors of Visual Popularity Prediction. <br/>Spencer Cappallo, Thomas Mensink and Cees Snoek. </td></tr>
  <tr><th colspan="2">Special Oral Session: Weakly Supervised Learning for Big Multimedia Data Analysis</th></tr>
- <tr><td>223</td><td>Harvesting Multiple Sources for User Profile Learning: a Big Data Study. Aleksandr Farseev, Mohammad Akbari and Tat-Seng Chua. </td></tr>
- <tr><td>179</td><td>Online Multi-modal Co-indexing and Retrieval for Weakly Supervised Web Image Collections. Lei Meng and Chunyan Miao. </td></tr>
- <tr><td>217</td><td>Weakly Supervised Random Forest for Multi-Label Image Clustering and Segmentation. Yingjie Xia, Qianqian Zhu and Wei Wei. </td></tr>
- <tr><td>128</td><td>Attribute Guided Dictionary Learning. Wei Wang, Yan Yan and Nicu Sebe</td></tr>
+ <tr><td>223</td><td>Harvesting Multiple Sources for User Profile Learning: a Big Data Study. <br/>Aleksandr Farseev, Mohammad Akbari and Tat-Seng Chua. </td></tr>
+ <tr><td>179</td><td>Online Multi-modal Co-indexing and Retrieval for Weakly Supervised Web Image Collections. <br/>Lei Meng and Chunyan Miao. </td></tr>
+ <tr><td>217</td><td>Weakly Supervised Random Forest for Multi-Label Image Clustering and Segmentation. <br/>Yingjie Xia, Qianqian Zhu and Wei Wei. </td></tr>
+ <tr><td>128</td><td>Attribute Guided Dictionary Learning. <br/>Wei Wang, Yan Yan and Nicu Sebe</td></tr>
 </table>
 
           </div>
