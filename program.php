446e855a50e5d637138053c7d2c0d76ff2c226f6
Xi Wang <wandsea@gmail.com>
2015-03-27 13:02:50 +0800

    program page init

---
+++ program.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Program | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Program</h1>
+          </div>
+          <div class="panel-body" >
+            <h3>Program at a glance</h3>
+<table class="table table-hover table-bordered ">
+<tr class="info"><th>Venue</th><th>Fudan University</th><th>International Convention Center</th><th>International Convention Center</th><th>International Convention Center</th></tr>
+ <tr><td>Date</td><td>June 23 Tuesday</td><td>June 24 Wednesday</td><td>June 25 Thursday</td><td>June 26 Friday</td></tr>
+ <tr><td>&nbsp;</td><td>Day 0: </td><td>Day 1: </td><td>Day 2: </td><td>Day 3: Main Conference &</td></tr>
+ <tr><td>&nbsp;</td><td>Tutorials & Workshops</td><td>Main Conference</td><td>Main Conference</td><td>Practitioner Day</td></tr>
+ <tr><td>08:30am</td><td>&nbsp;</td><td>Registration</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>08:45am</td><td>Registration</td><td>Welcome and Introduction</td><td>Registration</td><td>Registration</td></tr>
+ <tr><td>09:00am</td><td>TBA</td><td>Keynote 1</td><td>Keynote 2</td><td>Keynote 3 </td></tr>
+ <tr><td>09:20am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>09:40am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>10:00am</td><td>&nbsp;</td><td>Coffee Break</td><td>Coffee Break</td><td>Coffee Break </td></tr>
+ <tr><td>10:20am</td><td>&nbsp;</td><td>Oral 1: Image Retrieval</td><td>Oral 4: Analytical Methods for MR</td><td>Invited Talks from </td></tr>
+ <tr><td>10:40am</td><td>&nbsp;</td><td>5 papers </td><td>5 papers</td><td>Chinese Internet Industry</td></tr>
+ <tr><td>11:00am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>TBA</td></tr>
+ <tr><td>11:20am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>11:40am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>12:00pm</td><td>&nbsp;</td><td>Lunch</td><td>Lunch</td><td>Lunch</td></tr>
+ <tr><td>12:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>12:40pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>1:00pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>1:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>1:40pm</td><td>&nbsp;</td><td>Oral 2: Person and Objects</td><td>Oral 5: Best Paper Session</td><td>Oral 6: Photo Applications</td></tr>
+ <tr><td>2:00pm</td><td>&nbsp;</td><td>4 papers</td><td>4 papers</td><td>4 papers </td></tr>
+ <tr><td>2:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>2:40pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>3:00pm</td><td>&nbsp;</td><td>Coffee Break</td><td>Coffee Break</td><td>Coffee Break </td></tr>
+ <tr><td>3:20pm</td><td>&nbsp;</td><td>Oral 3: Concepts for MR</td><td>Panel Discussion</td><td>Full Paper Posters &</td></tr>
+ <tr><td>3:40pm</td><td>&nbsp;</td><td>4 papers</td><td>TBA</td><td>Special Poster Session</td></tr>
+ <tr><td>4:00pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>4:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>4:40pm</td><td>&nbsp;</td><td>Short Paper Posters & Demos</td><td>Special Oral Session</td><td>&nbsp;</td></tr>
+ <tr><td>5:00pm</td><td>&nbsp;</td><td>&nbsp;</td><td>4 papers</td><td>&nbsp;</td></tr>
+ <tr><td>5:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>5:40pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>6:00pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Conference Close</td></tr>
+ <tr><td>6:20pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>6:40pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>7:00pm--late</td><td>&nbsp;</td><td>Reception: Int'l Convention Center</td><td>Banquet: venue TBA</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td></td></tr>
+</table>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
