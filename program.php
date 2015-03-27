33ba211ccd4bf50d9c8f7a8919a628b8b64dd41b
Xi Wang <wandsea@gmail.com>
2015-03-27 14:03:35 +0800

    ...

--- program.php
+++ program.php
@@ -60,34 +60,34 @@
  <tr><td>136</td><td>Unified YouTube Video Recommendation via Cross-network Collaboration. Ming Yan, Jitao Sang and Changsheng Xu.</td></tr>
  <tr><td>15</td><td>Image Classification and Retrieval are ONE. Lingxi Xie, Richang Hong, Bo Zhang and Qi Tian.</td></tr>
  <tr><td>74</td><td>Social Event Mining in Large Photo Collections. Maia Zaharieva, Matthias Zeppelzauer, Manfred Del Fabro and Daniel Schopfhauser.</td></tr>
- <tr><td colspan="2" >Oral 1: Image Retrieval </td></tr>
+ <tr><th colspan="2" >Oral 1: Image Retrieval </th></tr>
  <tr><td>173</td><td>Fusing Pointwise and Pairwise Labels for Supporting Personalized Image Retrieval.Lin Chen and Baoxin Li.</td></tr>
  <tr><td>127</td><td>Effective, Efficient, and Scalable Unsupervised Distance Learning in Image Retrieval Tasks. Lucas Valem, Daniel C. G. Pedronette, Ricardo Torres, Edson Borin and Jurandy Almeida. </td></tr>
  <tr><td>138</td><td>Twin Feature and Similarity Maximal Matching for Image Retrieval. Lei Wang, Hanli Wang and Fengkuangtian Zhu. </td></tr>
  <tr><td>29</td><td>Fast Democratic Aggregation and Query Fusion for Image Search. Zhanning Gao, Jianru Xue, Wengang Zhou, Shanmin Pang and Qi Tian. </td></tr>
  <tr><td>51</td><td>DeepIndex for Accurate and Efficient Image Retrieval. Yu Liu and Michael S. Lew. </td></tr>
- <tr><td colspan="2">Oral 2: Person and Objects</td></tr>
+ <tr><th colspan="2">Oral 2: Person and Objects</th></tr>
  <tr><td>118</td><td>Extracting 3D Trajectories of Objects from 2D Videos using Particle Filter. Zeyd Boukhers, Kimiaki Shirahama, Frédéric Li and Marcin Grzegorzek. </td></tr>
  <tr><td>71</td><td>Facial Action Unit Classification with Hidden Knowledge under Incomplete Annotation. Jun Wang, Shangfei Wang and Qiang Ji.</td></tr>
  <tr><td>131</td><td>Temporal-Order Preserved Dynamic Quantization for Human Action Recognition from Multimodal Sensor Streams. Jun Ye, Guo-Jun Qi and Kien Hua. </td></tr>
  <tr><td>123</td><td>Space-time histograms and their application to person re-identification in TV shows. Rémi Auguste, Jean Martinet and Pierre Tirilly. </td></tr>
- <tr><td colspan="2">Oral 3: Concepts</td></tr>
+ <tr><th colspan="2">Oral 3: Concepts</th></tr>
  <tr><td>246</td><td>Encoding Concept Prototypes for Video Event Detection and Summarization. Masoud Mazloom, Amirhossein Habibian, Dong Liu, Cees Snoek and Shih-Fu Chang. </td></tr>
  <tr><td>83</td><td>Robust and Discriminative Concept Factorization for Image Representation. Yuchen Guo and Guiguang Ding. </td></tr>
  <tr><td>49</td><td>Fine-Grained Image Categorization by Localizing Tiny Object Parts from Unannotated Images. Luming Zhang, Yi Yang and Roger Zimmermann. </td></tr>
  <tr><td>247</td><td>Discovering Semantic Vocabularies for Cross-Media Retrieval. Amirhossein Habibian, Thomas Mensink and Cees Snoek. </td></tr>
- <tr><td colspan="2">Oral 4: Analytical</td></tr>
+ <tr><th colspan="2">Oral 4: Analytical</th></tr>
  <tr><td>222</td><td>Diffusion-on-Manifold Aggregation of Local Features for Shape-based 3D Model Retrieval. Takahiko Furuya and Ryutarou Ohbuchi. </td></tr>
  <tr><td>26</td><td>Nonnegative Sparse Neighborhood Propagation. Zhao Zhang, Mingbo Zhao, Li Zhang and Li Fanzhang. </td></tr>
  <tr><td>202</td><td>High-Dimensional Indexing by Sparse Approximation. Pedro Borges, André Mourão and Joao Magalhaes. </td></tr>
  <tr><td>111</td><td>Kernelizing Spatially Consistent Visual Matches for Fine-Grained Classification. Valentin Leveau, Alexis Joly, Olivier Buisson and Patrick Valduriez.</td></tr>
  <tr><td>58</td><td>Hierarchical Encoding of Binary Descriptors for Image Matching. Zhendong Mao, Yongdong Zhang and Qi Tian. </td></tr>
- <tr><td colspan="2">Oral 5: Photo Applications</td></tr>
+ <tr><th colspan="2">Oral 5: Photo Applications</th></tr>
  <tr><td>33</td><td>Bundling Centre For Landmark Image Discovery. Qian Zhang and Guoping Qiu. </td></tr>
  <tr><td>253</td><td>Visual Event Summarization on Social Media using Topic Modelling and Graph-based Ranking Algorithms. Manos Schinas, Symeon Papadopoulos, Yiannis Kompatsiaris and Pericles Mitkas. </td></tr>
  <tr><td>213</td><td>To Keep or not to Keep: An Expectation-oriented Photo Selection Method for Personal Photo Collections. Andrea Ceroni, Vassilios Solachidis, Claudia Niederée, Olga Papadopoulou, Nattiya Kanhabua and Vasileios Mezaris. </td></tr>
  <tr><td>249</td><td>Latent Factors of Visual Popularity Prediction. Spencer Cappallo, Thomas Mensink and Cees Snoek. </td></tr>
- <tr><td colspan="2">Special Oral Session: Weakly Supervised Learning for Big Multimedia Data Analysis</td></tr>
+ <tr><th colspan="2">Special Oral Session: Weakly Supervised Learning for Big Multimedia Data Analysis</th></tr>
  <tr><td>223</td><td>Harvesting Multiple Sources for User Profile Learning: a Big Data Study. Aleksandr Farseev, Mohammad Akbari and Tat-Seng Chua. </td></tr>
  <tr><td>179</td><td>Online Multi-modal Co-indexing and Retrieval for Weakly Supervised Web Image Collections. Lei Meng and Chunyan Miao. </td></tr>
  <tr><td>217</td><td>Weakly Supervised Random Forest for Multi-Label Image Clustering and Segmentation. Yingjie Xia, Qianqian Zhu and Wei Wei. </td></tr>
