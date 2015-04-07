80932de129c9e44241cf1eec2e7d930a2a6d2be6
Xi Wang <wandsea@gmail.com>
2015-04-07 17:16:34 +0800

    ...

--- php/sidebar.php
+++ php/sidebar.php
@@ -5,13 +5,13 @@
             </div>
             <div class="panel-body" >
               <p class="text-center" style="max-width: 300px">
-                <img class="img-responsive" src="img/sponsor-tencent.png" style="margin-bottom: 20px;">
-                <img class="img-responsive" src="img/sponsor-samsung.png" style="margin-bottom: 20px;">
-                <img class="img-responsive" src="img/sponsor-google.png" style="margin-bottom: 20px;">
-                <img class="img-responsive" src="img/sponsor-visenze.png" style="margin-bottom: 20px;">
-                <img class="img-responsive" src="img/acm_logo.gif" style="margin-bottom: 20px;">
-                <img class="img-responsive" src="img/sigmm.jpg" style="margin-bottom: 20px;">
-                <img src="img/fudan.png" style="max-height: 80px; margin-bottom: 20px;">
+                <img class="img-responsive" src="img/sponsor-tencent.png" style="padding-bottom: 20px;">
+                <img class="img-responsive" src="img/sponsor-samsung.png" style="padding-bottom: 20px;">
+                <img class="img-responsive" src="img/sponsor-google.png" style="padding: 0px 25px 20px 25px;">
+                <img class="img-responsive" src="img/sponsor-visenze.png" style="padding: 0px 25px 20px 25px;">
+                <img class="img-responsive" src="img/acm_logo.gif" style="padding-bottom: 20px;">
+                <img class="img-responsive" src="img/sigmm.jpg" style="padding-bottom: 20px;">
+                <img src="img/fudan.png" style="max-height: 80px; padding-bottom: 20px;">
              </p>
             </div>
           </div>
