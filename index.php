879e7a66b544e2a2902b15bdc3398c8ec8a1d51f
Xi Wang <wandsea@gmail.com>
2015-04-07 17:06:40 +0800

    ...

--- index.php
+++ index.php
@@ -82,8 +82,8 @@
           <p class="text-center">
           <img src="img/sponsor-tencent.png" style="height: 45px; padding-right: 20px;">
           <img src="img/sponsor-samsung.png" style="height: 65px; padding-right: 20px;">
-          <img src="img/sponsor-google.png" style="height: 60px;">
-          <img src="img/sponsor-visenze.png" style="height: 60px;">
+          <img src="img/sponsor-google.png" style="height: 55px; padding-right: 20px;">
+          <img src="img/sponsor-visenze.png" style="height: 50px; padding-bottom: 10px;">
           
           </p>
           <p class="text-center">
