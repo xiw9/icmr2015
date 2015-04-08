8054463d4d1dea479698e0a121c1f79c155c02ca
Xi Wang <wandsea@gmail.com>
2015-04-08 14:02:54 +0800

    ...

---
+++ registration.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Registration | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Registration</h1>
+          </div>
+          <div class="panel-body" >
+
+
+<table class="table table-bordered">
+<tr><th>Early Birds (On or before May 19, 2015)</th>
+<th>ACM Member</th><th>Non-ACM Member</th><th>Student Member</th></tr>
+<tr><td>Main Conference</td><td>$600.00</td><td>$700.00</td><td>$300.00</td></tr>
+<tr><td>Workshop</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+<tr><td>Tutorial</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+<tr><th>Regular (During Dec 20, 2014 and Jan 30, 2015)</th>
+<th>ACM Member</th><th>Non-ACM Member</th><th>Student Member</th></tr>
+<tr><td>Main Conference</td><td>$600.00</td><td>$700.00</td><td>$300.00</td></tr>
+<tr><td>Workshop</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+<tr><td>Tutorial</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+<tr><th>On-site</th>
+<th>ACM Member</th><th>Non-ACM Member</th><th>Student Member</th></tr>
+<tr><td>Main Conference</td><td>$600.00</td><td>$700.00</td><td>$300.00</td></tr>
+<tr><td>Workshop</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+<tr><td>Tutorial</td><td>$150.00</td><td>$200.00</td><td>$100.00</td></tr>
+</table>
+
+<h3>Date and Location</h3>
+<p>Tuesday, June 23, 2015<br />
+Fudan University<br />
+220 Handan Rd, Shanghai, China<br />
++86 21 6564 2222 </p>
+<p>Wednesday, June 24, 2015 &#8211; Friday, June 26, 2015<br/>
+Shanghai International Convention Center <br/>
+2727 Binjiang Ave, Pudong, Shanghai, China <br/>
++86 21 5037 0000</p>
+
+
+<h3>Invitation Letter or Visa Support Letter</h3>
+<p>For those who need a visa to travel in China, we can issue an <strong>invitation letter</strong> or a <strong>visa support letter</strong> for your visa application. Visa application process varies at the Chinese embassies/consulates in different countries, and the requirements may even vary for your different personal situations. Please consult with the Chinese embassy/consulate where you are going to apply for the visa, if possible.</p>
+<p>Please refer to the <a title="Visa Information" href="visa-information">visa information page</a> for details.</p>
+
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
