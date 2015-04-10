ded99dc1438d637e54b40cf9463c529895e5b2cf
Xi Wang <wandsea@gmail.com>
2015-04-10 10:59:36 +0800

    ...

--- visa-information.php
+++ visa-information.php
@@ -19,7 +19,7 @@
           <div class="panel-body" >
 <p>For those who need a visa to travel in China, we can issue an <strong>invitation letter</strong> or a <strong>visa support letter</strong> for your visa application. Visa application process varies at the Chinese embassies/consulates in different countries, and the requirements may even vary for your different personal situations. Please consult with the Chinese embassy/consulate where you are going to apply for the visa, if possible.</p>
 <h3>Invitation Letter</h3>
-<p>This is issued by the conference organization committee, which is an ordinary conference invitation letter. This is a quick and easy method, and the experiences indicate that this is good enough for most situations and most locations for applying for the visa. If you have completed the registration, you may request this invitation letter by sending an email to ??? (<a href="mailto:???@???.com">???@???.com</a>), including the following information:</p>
+<p>This is issued by the conference organization committee, which is an ordinary conference invitation letter. This is a quick and easy method, and the experiences indicate that this is good enough for most situations and most locations for applying for the visa. If you have completed the registration, you may request this invitation letter by sending an email to Dr. Cheng Jin (<a href="mailto:mci@fudan.edu.cn">mci@fudan.edu.cn</a>), including the following information:</p>
 <ul>
 <li>Your name;</li>
 <li>If you are an author of a paper accepted for the conference or a workshop, please provide the workshop name (if relevant), paper ID and the title;</li>
@@ -28,7 +28,7 @@
 <li>Also: <strong>your birthday, gender, and passport number</strong>.</li>
 </ul>
 <h3>Official Visa Support Letter</h3>
-<p>This is issued by the Foreign Affair Office of Fudan University (the official sponsoring host institution of ICMR 2015), which is a formal official visa support letter. In case where the visa application policy is rather restrictive at the Chinese embassy/consulate in your region/country, we suggest you choose this method. And please send your request email to ??? (<a href="mailto:???@???.com">???@???.com</a>) as early as possible, since we need more time to process this type of letters. If you think you need a copy of the official visa support letter, please include the following information in the email:</p>
+<p>This is issued by the Foreign Affair Office of Fudan University (the official sponsoring host institution of ICMR 2015), which is a formal official visa support letter. In case where the visa application policy is rather restrictive at the Chinese embassy/consulate in your region/country, we suggest you choose this method. And please send your request email to Dr. Cheng Jin <a href="mailto:mci@fudan.edu.cn">mci@fudan.edu.cn</a>) as early as possible, since we need more time to process this type of letters. If you think you need a copy of the official visa support letter, please include the following information in the email:</p>
 <ul>
 <li>Your name (as it appears on your passport) and mailing address;</li>
 <li>A copy of the first page (that includes your photo and passport number ) of your passport;</li>
