b5798f59c806f48f5863807ee9744559b9c6c91b
Xi Wang <wandsea@gmail.com>
2015-04-14 18:43:25 +0800

    ...

--- checkout.php
+++ checkout.php
@@ -19,11 +19,6 @@
           </div>
           <div class="panel-body" >
             <p>The first step of your registration is now a 'success', now you can complete the second step (the payment step), If there is any problem with the registration information, please contact <a href='mailto:xwang10@fudan.edu.cn' ><b>us</b></a></p>
-            <p>Registration ID: <?php echo $reg_longid; ?></p>
-            <p>Name: <?php echo $first_name.$last_name; ?></p>
-            <p>Email: <?php echo $email; ?></p>
-            <p>Registration Type:  <?php case($reg_type) {case 1: echo "Main Conference"; break; case 2: echo "Workshops"; break; case 3: echo "Tutorials"; break; echo "Error, please try again.";} ?></p>
-            <p>Member Type: <?php case($member_type) {case 1: echo "ACM Member"; break; case 2: echo "Non-ACM Member"; break; case 3: echo "Student"; break; echo "Error, please try again." ;} ?></p>
 
           </div>
         </div>  
