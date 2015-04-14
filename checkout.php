0e831024c4d049aafbe871a60d6a5f671dd21064
Xi Wang <wandsea@gmail.com>
2015-04-14 18:47:27 +0800

    ...

--- checkout.php
+++ checkout.php
@@ -19,11 +19,9 @@
           </div>
           <div class="panel-body" >
             <p>The first step of your registration is now a 'success', now you can complete the second step (the payment step), If there is any problem with the registration information, please contact <a href='mailto:xwang10@fudan.edu.cn' ><b>us.</b></a></p>
-            <p>Registration ID: <?php echo $reg_longid; ?></p>
-            <p>Name: <?php echo $first_name.$last_name; ?></p>
-            <p>Email: <?php echo $email; ?></p>
-
-
+            <p>Registration ID: <?php echo($reg_longid); ?></p>
+            <p>Name: <?php echo($first_name.$last_name); ?></p>
+            <p>Email: <?php echo($email); ?></p>
           </div>
         </div>  
       </div>
