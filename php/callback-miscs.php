c499e6ff84b9665b3be5fb62a163223e15b060a6
Xi Wang <wandsea@gmail.com>
2015-04-15 00:39:01 +0800

    ...`

--- php/callback-miscs.php
+++ php/callback-miscs.php
@@ -72,7 +72,7 @@ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
 // Tell curl not to return headers, but do return the response
 curl_setopt($session, CURLOPT_HEADER, false);
 // Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_DEFAULT);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
 // obtain response
