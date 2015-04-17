7128d2ef78d18a6f384c767628368bc71517f64a
Xi Wang <wandsea@gmail.com>
2015-04-17 15:15:36 +0800

    ...

---
+++ accommodation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Accommodation | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Accommodation</h1>
+          </div>
+          <div class="panel-body" >
+            <h3>Accommodation Options</h3>
+            <table class="table table-striped">
+              <tr><th>Hotel</th><th>Location</th><th>Phone</th><th></th></tr>
+              <tr><td><a href="http://www.shicc.net/shicc/" target="_blank">Oriental Riverside Hotel Shanghai (special rates)</a></td><td>2727 Binjiang Avenue, Pudong</td><td>+8621-50370000</td><td>★★★★★</td></tr>
+              <tr><td><a href="http://www.shangri-la.com/shanghai/pudongshangrila/" target="_blank">Pudong Shangri-La Shanghai (special rates)</a></td><td>33 Fucheng Road, Pudong</td><td>+8621-68828888</td><td>★★★★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-346412/" target="_blank">The Ritz-Carlton Shanghai, Pudong</a></td><td>8 Century Avenue, Pudong</td><td>+8621-20201888</td><td>★★★★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-425821/" target="_blank">Grand Kempinski Hotel Shanghai</a></td><td>Lujiazui Ring Road 1288, Pudong</td><td>+8621-38678888</td><td>★★★★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-444194/" target="_blank">Shanghai Baron Business Hotel</a></td><td>105-1 Fuzhou Road,Huangpu</td><td>+8621-31012330</td><td>★★★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-375466/" target="_blank">Manhattan Bund Business Hotel</a></td><td>81-85 Dianchi Road,Huangpu</td><td>+8621-68888123</td><td>★★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-1487417/" target="_blank">Hanting Express (Shanghai East Nanjing Road Pedestrian Street)</a></td><td>505 Middle He’nan Road, Huangpu</td><td>+8621-63600088</td><td>★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-419388/" target="_blank">Motel 168 (Shanghai Bund Nanjing Road Pedestrian Street)</a></td><td>50 Ningbo Road, Huangpu</td><td>+8621-51793333</td><td>★★</td></tr>
+              <tr><td><a href="http://english.ctrip.com/hotels/shanghai-hotel-detail-428122/" target="_blank">Home Inn (Shanghai Nanjing Road Pedestrian Street of Shanxi Road)</a></td><td>218 South Shanxi Road, Huangpu</td><td>+8621-33665999</td><td>★★</td></tr>
+            </table>
+            <p>For more options, please refer to <a href="http://english.ctrip.com/hotels/map?city=2&checkin=06-21-2015&checkout=06-27-2015&hotelname=&searchboxArg=t&optionId=426312&optionType=globalhotel_hotellang&display=Oriental%20Riverside%20Bund%20View%20Hotel%2C%20Shanghai%2C%20China&markland=">Ctrip</a> for more information.</p>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
