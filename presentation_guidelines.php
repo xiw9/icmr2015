a15258ebf25a048cf5860a900ec3020079f9fc98
Xi Wang <wandsea@gmail.com>
2015-04-18 21:17:32 +0800

    ...

---
+++ presentation_guidelines.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Presentation Guidelines | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">Presentation Guidelines</h1>
+          </div>
+          <div class="panel-body" >
+            <p>Presenters are requested to prepare their presentations as follows. 
+              Please carefully refer to the <a href="program">program</a> to check the presentation date, time, and type. 
+              For workshops, please follow the instructions from the workshop organizers.</p>
+            <h3>Oral Presentations</h3>
+            <p>Each paper in an oral session is allocated 20 minutes. 
+              This includes time required for the introduction of the speaker, as well as time for questions from the audience. 
+              Therefore authors are advised to prepare a 15-minute talk and leave time for questions at the end.</p>
+            <p>Presenters are requested to be present at the podium at least 15 minutes before the start of their session and 
+              introduce themselves to the session chair as soon as they arrive. 
+              All oral session rooms are equipped with PC projectors with a standard VGA connector. 
+              Presenters are requested to bring their own PC for the presentation. 
+              We strongly recommend presenters to check the compatibility of their PC and the projector before the session starts. </p>
+            <h3>Poster Presentations</h3>
+            <p>Each paper in a poster session will be provided with a board (size will be announced later)</a>. 
+              The poster board size is a hard limit, overflow space cannot be guaranteed.</p>
+            <p><b>A presenting author is expected to be available in front of the poster for the entire duration of the poster session!</b></p>
+            <h3>Demo Presentations</h3>
+            <p>Each demonstrator will be provided with a table (size TBA), a power outlet (220V), and wireless internet access. 
+              If your demo has special requirements that exceed this, please contact the Demo Chairs as soon as possible.</p>
+            <p>Each demo should be set-up before the session (e.g., during the coffee break) and removed after the session. 
+              Demonstrators must be in the demo space and present their demonstrations during the session-time.</p>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
