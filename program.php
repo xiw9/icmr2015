67fc0672742d80496edb46b4bbd8e5ad79696fed
Xi Wang <wandsea@gmail.com>
2015-04-24 13:37:03 +0800

    ...

--- program.php
+++ program.php
@@ -33,8 +33,8 @@
  <tr><td>11:00am</td><td class="info">&nbsp;</td><td class="info">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="warning">&nbsp;</td></tr>
  <tr><td>11:20am</td><td class="info">&nbsp;</td><td class="info">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="warning">&nbsp;</td></tr>
  <tr><td>11:40am</td><td class="info">&nbsp;</td><td class="info">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="warning">&nbsp;</td></tr>
- <tr height="35"><td>12:00pm<br/>--<br/>1:00pm</td><td class="info">&nbsp;</td><td>Lunch (on your own)</td><td>Lunch</td><td>Lunch</td><td>Lunch</td></tr>
- <tr height="35"><td>1:00pm<br/>--<br/>1:40pm</td><td>Lunch (on your own)</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr height="40"><td>12:00pm<br/>--<br/>1:00pm</td><td class="info">&nbsp;</td><td>Lunch<br/>(on your own)</td><td>Lunch</td><td>Lunch</td><td>Lunch</td></tr>
+ <tr height="30"><td>1:00pm<br/>--<br/>1:40pm</td><td>Lunch<br/>(on your own)</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
  <tr><td>1:40pm</td><td>&nbsp;</td><td>&nbsp;</td><td class="success"><a href="#oral2">Oral 2: Person and Objects</a></td><td class="success"><a href="#best">Best Paper Session</a></td><td class="success"><a href="#oral5">Oral 5: Photo Applications</a></td></tr>
  <tr><td>2:00pm</td><td>&nbsp;</td><td class="info">Tutorial 1</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td></tr>
  <tr><td>2:20pm</td><td class="info">Tutorial 2</td><td class="info">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td><td class="success">&nbsp;</td></tr>
