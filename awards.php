55960e632d47df939c847624c180e027894965e3
Xi Wang <wandsea@gmail.com>
2015-07-31 01:58:26 +0800

    ...

---
+++ awards.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include 'php/header.php' ?>
+  <title>Best Paper Awards | ACM ICMR 2015 | Shanghai, China</title>
+</head>
+<body>
+  <?php include 'php/navbar.php' ?>
+  <div class="ord-header">
+    <img class="img-responsive" src="img/header.jpg" alt="Scenery of Shanghai">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h1 class="panel-title">ICMR 2015 Best Paper Awards</h1>
+          </div>
+          <div class="panel-body" >
+<h3>Best Paper Award</h3>
+<p>Image Classification and Retrieval are ONE</p>
+<p>Lingxi Xie, Richang Hong, Bo Zhang and Qi Tian.</p>
+<hr>
+
+<h3>Best Student Paper Award</h3>
+<p>Unified YouTube Video Recommendation via Cross-network Collaboration</p>
+<p>Ming Yan, Jitao Sang and Changsheng Xu.</p>
+<hr>
+
+<h3>Best Demo Award</h3>
+<p>Incremental Multimodal Query Construction for Video Search</p>
+<p>Shicheng Xu, Huan Li, Xiaojun Chang, Shoou-I Yu, Xingzhong Du, Xuanchong Li, Lu Jiang, Zexi Mao, Zhenzhong Lan, Susanne Burger and Alexander Hauptmann.</p>
+          </div>
+        </div>  
+      </div>
+      <div class="col-md-3">
+      <?php include 'php/sidebar.php' ?>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+  </div><!-- /.container -->
+  <?php include 'php/footer.php' ?>
+</body>
+</html>
